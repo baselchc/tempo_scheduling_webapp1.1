7018be94cd7e64491324217adde7f042ec5da662
DarrelNugget <darrelnguyen03@gmail.com>
2024-09-18 22:39:25 -0600

    adding more pages

--- app/employee/page.js
+++ app/employee/page.js
@@ -15,8 +15,9 @@ export default function EmployeePage() {
   ];
 
   return (
-    <div className="relative flex">
-      {/* Vertical Navbar */}
+    // Main Page Layout
+    <div className="relative flex bg-gray-400">
+      {/* Navigation Bar */}
       <nav className="bg-blue-500 h-screen w-1/4 flex flex-col justify-between p-4">
         <div>
           <Image
@@ -36,53 +37,34 @@ export default function EmployeePage() {
           </ul>
 
           <div className="flex items-center gap-4 fixed bottom-8 left">
-          <Image
-            className="rounded-full"
-            src={user?.profileImageUrl || '/default-avatar.png'}
-            alt="Profile image"
-            width={50}
-            height={50}
-          />
-          <div>
-            <div className="font-bold">{user?.emailAddresses[0].emailAddress}</div>
-            <div className="text-sm text-white">
-              ({user?.organizationMemberships?.[0]?.role === 'org:admin' ? 'Administrator' : 'Member'})
+            <Image
+              className="rounded-full"
+              src={user?.profileImageUrl || '/default-avatar.png'}
+              alt="Profile image"
+              width={50}
+              height={50}
+            />
+
+            <div>
+              {/* Maybe add just the name by itself or with email underneath it */}
+              <div className="font-bold">
+                {user?.emailAddresses[0].emailAddress}
+              </div> 
+              <div className="text-sm text-white">
+                ({user?.organizationMemberships?.[0]?.role === 'org:admin' ? 'Administrator' : 'Member'})
+              </div>
             </div>
-          </div>
-          <button
-            onClick={() => signOut()}
-            className="ml-1 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-12 w-40 px-5 "
-          >
-            Sign Out →
-          </button>
+
+            <button
+              onClick={() => signOut()}
+              className="ml-1 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center dark:hover:bg-gray-400 hover:border-transparent text-sm sm:text-base h-12 w-30 px-5 "
+            >
+              Sign Out →
+            </button>
           </div>
         </div>
-        
       </nav>
 
-      {/* Main Content */}
-      <div className="w-4/5 p-8">
-        <div className="text-right">
-          {user && (() => {
-            const userName = user.emailAddresses[0].emailAddress.split('@')[0];
-            const capitalizedUserName = userName.charAt(0).toUpperCase() + userName.slice(1);
-            return (
-              <>
-                <div className="text-2xl font-bold">
-                  Welcome to Tempo, {capitalizedUserName}!
-                </div>
-              </>
-            );
-          })}
-      </div>
-
-        {/* Shifts for the Next 7 Days */}
-        <div className="mt-8">
-          <h2 className="text-2xl font-semibold mb-4">Shift Info Here</h2>
-        </div>
-
-        
-      </div>
     </div>
   );
 }
