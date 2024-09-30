b395de5793e6a8efa40b737c01664274a2b7caa1
DarrelNugget <darrelnguyen03@gmail.com>
2024-09-30 00:08:45 -0600

    Availabilty for profile

--- app/employee/profile/page.js
+++ app/employee/profile/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React from 'react';
-import { useUser, useAuth } from '@clerk/nextjs';
+import { useUser } from '@clerk/nextjs';
 import Layout from '../Layout';
 export default function EmployeeProfile() {
   const { user } = useUser();
@@ -56,9 +56,67 @@ export default function EmployeeProfile() {
           
          
         </div>
-        
+        <div className="bg-white shadow-md rounded-lg p-4 mt-5">
+          <table className="min-w-full bg-white">
+            <thead className="text-left text-xl font-bold text-black">
+            Availability
+            </thead>
+            <tbody>
+              {/* First Row*/}
+              <tr>
+                <td className="w-1/3 pl-2 pr-2 pb-2">
+                  <div className="text-black bg-gray-200 shadow-md rounded-lg p-2 mt-2 text-left">
+                    Monday:
+                  </div>
+                </td>
+                <td className="w-1/3 pl-2 pr-2 pb-2">
+                  <div className=" text-black bg-gray-200 shadow-md rounded-lg p-2 mt-2 text-left">
+                    Tuesday:
+                  </div>
+                </td>
+                <td className="w-1/3 pl-2 pr-2 pb-2">
+                  <div className=" text-black bg-gray-200 shadow-md rounded-lg p-2 mt-2 text-left">
+                    Wednesday:
+                  </div>
+                </td>
+              </tr>
+              {/* Second Row*/}
+              <tr>
+                <td className="w-1/3 pl-2 pr-2 pb-2">
+                  <div className="text-black bg-gray-200 shadow-md rounded-lg p-2 mt-2 text-left">
+                    Thursday:
+                  </div>
+                </td>
+                <td className="w-1/3 pl-2 pr-2 pb-2">
+                  <div className=" text-black bg-gray-200 shadow-md rounded-lg p-2 mt-2 text-left">
+                    Friday:
+                  </div>
+                </td>
+                <td className="w-1/3 pl-2 pr-2 pb-2">
+                  <div className=" text-black bg-gray-200 shadow-md rounded-lg p-2 mt-2 text-left">
+                    Saturday:
+                  </div>
+                </td>
+              </tr>
+              {/* Third Row*/}
+              <tr>
+                <td className="w-1/3 pl-2 pr-2 pb-2">
+                  <div className="text-black bg-gray-200 shadow-md rounded-lg p-2 mt-2 text-left">
+                    Sunday:
+                  </div>
+                </td>
+                <td className="w-1/3 pl-2 pr-2 pb-2 ">
+                  <button className="min-w-full text-black bg-blue-400 shadow-md rounded-lg p-2 mt-2 text-center text-bold">
+                    Edit
+                  </button>
+                </td>
+               
+              </tr>
+            </tbody>
+          </table>
+        </div>
 
-        
+
       </div>
     </Layout>
   );
