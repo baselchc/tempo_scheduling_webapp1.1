b5bce173bdf1c6df0cc7a39c52af9c09200facdf
Darrel Nguyen <darrelnguyen03@gmail.com>
2024-10-15 14:57:15 -0600

    laptop to pc commit/ docker code and incomplete image files transfer

--- app/employee/page.js
+++ app/employee/page.js
@@ -30,12 +30,12 @@ export default function EmployeePage() {
   };
 
   return (
-    <div className="relative min-h-screen text-black">
+    <div className="relative min-h-screen text-black bg-white">
       {/* Blurred background image */}
       <div
         className="absolute inset-0 -z-10 bg-cover bg-center filter blur-2xl"
         style={{
-          backgroundImage: `url('/images/loginpagebackground.webp')`,
+          backgroundImage: `url('/images/loginpagebackground.png')`,
         }}
       ></div>
 
