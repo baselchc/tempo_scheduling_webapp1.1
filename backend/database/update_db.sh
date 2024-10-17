b8a346c04e47c336737c505d696a8e082a8b7c34
Vrajdp1 <vraj.d.patel@edu.sait.ca>
2024-10-16 18:40:32 -0600

    Update update_db.sh

--- backend/database/update_db.sh
+++ backend/database/update_db.sh
@@ -7,7 +7,7 @@ DB_PASSWORD="password"
 DB_NAME="Tempo"
 
 # Path to psql (updated based on your system)
-PSQL_PATH="/C/Program Files/PostgreSQL/17/bin/psql.exe"
+PSQL_PATH="/C/Program Files/PostgreSQL/16/bin/psql.exe"
 
 # Execute the schema.sql file
 PGPASSWORD="$DB_PASSWORD" "$PSQL_PATH" -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -f schema.sql
